90f84669395f0d80fbbf8916db99e645a1c69314
HypatiaLR <micahreckleindev@gmail.com>
2025-03-10 16:16:08 -0500

    updated stats.py to count chars, updated main.py to format and display all data

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from stats import book_word_count
+from stats import book_char_count
+from stats import book_sorted_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    book_text = get_book_text(filepath)
     num_words = book_word_count(book_text)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    char_count_dict = book_char_count(book_text)
+    sorted_char_list = book_sorted_chars(char_count_dict)
+    print("--------- Character Count -------")
+    for char in sorted_char_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["count"]}")
+    print("============= END ===============")
+    
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,19 @@
 def book_word_count(book_text):
     return len(book_text.split())
+
+def book_char_count(book_text):
+    lower_text = book_text.lower()
+    char_count_dict = {}
+    for char in lower_text:
+        if char not in char_count_dict:
+            char_count_dict[char] = 0
+        char_count_dict[char] = char_count_dict[char] + 1
+    
+    return char_count_dict
+
+def book_sorted_chars(char_dict):
+    char_list = []
+    for char in char_dict:
+        char_list.append({"char":char, "count":char_dict[char]})
+    char_list.sort(reverse=True, key=lambda dict:  dict["count"])
+    return char_list
