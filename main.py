b38b21981b601d47bcce00916ad0c222484850fb
HypatiaLR <micahreckleindev@gmail.com>
2025-03-10 15:44:51 -0500

    added .gitignore, main.py, stats.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import book_word_count
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    num_words = book_word_count(book_text)
+    print(f"{num_words} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def book_word_count(book_text):
+    return len(book_text.split())
