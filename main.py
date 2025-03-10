242ad73600ab21e62a7f5a1d374e13a6cf7d80de
HypatiaLR <micahreckleindev@gmail.com>
2025-03-10 16:29:12 -0500

    updated main.py to take CLI input

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import book_word_count
 from stats import book_char_count
 from stats import book_sorted_chars
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     book_text = get_book_text(filepath)
